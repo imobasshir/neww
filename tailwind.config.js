58778bf0cdced9a442e28b93c58941ae4a8c77b6
Mobasshir Imam <iibrahimimam24@gmail.com>
2024-09-07 01:24:21 +0530

    Refactor responsive design and add Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   darkMode: 'class', // Enables dark mode via a class
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      vs: '320px',
+      xxs: '460px',
+      xs: '580px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      my: '1200px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       colors: {
         background: {
