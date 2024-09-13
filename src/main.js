90ed63f6c649a789c159d49d633c01a593c02a22
Mobasshir Imam <iibrahimimam24@gmail.com>
2024-09-13 23:51:05 +0530

    Refactor responsive design, update Tailwind CSS configuration, and add navigation functionality

--- src/main.js
+++ src/main.js
@@ -5,3 +5,27 @@ const toggleDarkMode = () => {
 const toggleLightMode = () => {
     document.documentElement.classList.remove('dark');
 }
+
+const nav = document.querySelector('.nav');
+const navList = nav.querySelectorAll('li');
+const pages = document.querySelectorAll('article');
+const mainContact = document.querySelector('.main-contact');
+
+pages[0].classList.remove('hidden');
+
+navList.forEach((item) => {
+    item.addEventListener('click', () => {
+        navList.forEach((navItem) => {
+            navItem.classList.remove('text-primary', 'font-medium');
+            navItem.classList.add('text-gray-950');
+        });
+        item.classList.remove('text-gray-950');
+        item.classList.add('text-primary', 'font-medium');
+
+        mainContact.scrollIntoView({ behavior: 'smooth' });
+        
+        pages.forEach((page) => {
+            page.classList.contains(item.id) ? page.classList.remove('hidden') : page.classList.add('hidden');
+        });
+    });
+});
